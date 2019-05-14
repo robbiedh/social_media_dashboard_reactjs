e56b96eb4c71e42ca1c6bb08f57f1dcf3ecae3b8
root <root@6cea0f54ee0b>
2019-05-14 06:33:51 +0000

    react-create-app on Heroku

---
+++ build/precache-manifest.64278a2558cbf53fc900c27afbe20631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01dff7181383a97fb96299cdf3449d8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ec106f02ccab7ea8180",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6001d186fb42f9de0c40",
+    "url": "/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "7ec106f02ccab7ea8180",
+    "url": "/static/js/2.126f266f.chunk.js"
+  },
+  {
+    "revision": "6001d186fb42f9de0c40",
+    "url": "/static/js/main.e131d557.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
